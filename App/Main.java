57daadd9dde4c1be3a0e06522e7dc44a29552077
Mahbubur Rahman Sayem <sayemmahbub7@gmail.com>
2026-06-04 15:58:39 +0600

    Create Main.java

---
+++ App/Main.java
@@ -0,0 +1,9 @@
+package App;
+import Services.CricketSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        CricketSystem system = new CricketSystem();
+        system.start();
+    }
+}

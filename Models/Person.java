03d44fbfd76c07dfcfec5f3525f628e46ff0f51d
Mahbubur Rahman Sayem <sayemmahbub7@gmail.com>
2026-06-04 15:59:22 +0600

    Create Person.java

---
+++ Models/Person.java
@@ -0,0 +1,13 @@
+package Models;
+
+public abstract class Person {
+    protected int id;
+    protected String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public abstract void display();
+}
